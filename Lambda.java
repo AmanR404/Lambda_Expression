3422223ec3ca8602bda7f819775803dc2a0eabe9
Aman <raiaman21@gmail.com>
2022-08-04 14:34:21 +0530

    UC4 Solved

--- Lambda.java
+++ Lambda.java
@@ -5,16 +5,14 @@ interface NumberPlayList{
 
 public class Lambda{
     public static void main(String[] args) {
-        int[] array = {2,5,65,87};
         NumberPlayList playlist = new NumberPlayList() {
 
             @Override
             public void meth(int[] array) {
                 for (int element : array) {
-                    System.out.println(element);                    
+                    System.out.println(element*element);                    
                 }
-            }
-            playlist.meth(array);  
+            }  
         };
     }
 }
