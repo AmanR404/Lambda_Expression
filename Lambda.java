29edbcedcadfd38ece792e98df9ceccdb8593f8c
Aman <raiaman21@gmail.com>
2022-08-04 14:31:17 +0530

    UC3 Solved

--- Lambda.java
+++ Lambda.java
@@ -1,36 +1,20 @@
 @FunctionalInterface
-interface Addition{
-    void meth(int a, int b);
+interface NumberPlayList{
+    void meth(int[] array);
 }
-@FunctionalInterface
-interface Subtraction{
-    void meth(int a, int b);
-}
-
-@FunctionalInterface
-interface Division{
-    void meth(int a, int b);
-}
-
 
 public class Lambda{
     public static void main(String[] args) {
-        Addition add = (a,b)->{
-            System.out.println("Addition is " + a+b);
-        };
+        int[] array = {2,5,65,87};
+        NumberPlayList playlist = new NumberPlayList() {
 
-        Subtraction sub = (a,b)->{
-            System.out.print("Subtraction is ");
-            System.out.println(a-b);
+            @Override
+            public void meth(int[] array) {
+                for (int element : array) {
+                    System.out.println(element);                    
+                }
+            }
+            playlist.meth(array);  
         };
-
-        Division div = (a,b)->{
-            System.out.println("Division is " + a/b);
-        };
-
-        add.meth(2, 4);
-        sub.meth(4, 2);
-        div.meth(10, 2);
     }
-
 }
